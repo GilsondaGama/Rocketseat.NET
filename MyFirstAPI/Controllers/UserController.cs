5514c3cf18a5fdbbaf8cc39171836a3bc8b2b9f7
Gilson da Gama <gilsondagama@hotmail.com>
2025-11-10 12:13:51 -0300

    Criando meu primeiro endpoint

---
+++ MyFirstAPI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUser()
+    {
+        return Ok("User data");
+    }
+}
+
