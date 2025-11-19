3d9973b97b7ecca2008089e67e4a33820bfa9828
Gilson da Gama <gilsondagama@hotmail.com>
2025-11-19 09:28:45 -0300

    TaskManager: Endpoint para criação de tarefas

---
+++ TaskManager/TaskManager.API/Controllers/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.UseCases.Tasks.Register;
+using TaskManager.Communication.Requests;
+using TaskManager.Communication.Responses;
+
+namespace TaskManager.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TaskController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterTaskJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+
+    public IActionResult RegisterTask([FromBody] RequestTaskJson request)
+    {
+        var useCase = new RegisterTaskUseCase();
+        var response = useCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+}
---
+++ TaskManager/TaskManager.Application/UseCases/Tasks/Register/RegisterTaskUseCase.cs
@@ -0,0 +1,21 @@
+﻿using TaskManager.Communication.Requests;
+using TaskManager.Communication.Responses;
+
+namespace TaskManager.Application.UseCases.Tasks.Register;
+
+public class RegisterTaskUseCase
+{
+    public ResponseRegisterTaskJson Execute(RequestTaskJson request)
+    {
+
+        return new ResponseRegisterTaskJson
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Description = request.Description,
+            Priority = request.Priority,
+            DueDate = request.DueDate,
+            Status = request.Status
+        };
+    }
+}
