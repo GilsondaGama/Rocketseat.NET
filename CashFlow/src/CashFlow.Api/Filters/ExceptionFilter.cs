759e8a2122cb2e5a596be1a64df14152a0b5d049
Gilson da Gama <gilsondagama@hotmail.com>
2025-12-03 10:19:26 -0300

    CashFlow: Configurando e testando o filtro

--- CashFlow/src/CashFlow.Api/Filters/ExceptionFilter.cs
+++ CashFlow/src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -30,6 +30,13 @@ public class ExceptionFilter : IExceptionFilter
             context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
             context.Result = new ObjectResult(errorResponse);
         }
+        else
+        {
+            var errorResponse = new ResponseErrorJson(context.Exception.Message);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new ObjectResult(errorResponse);
+        }
     }
 
     private void ThrowUnkowError(ExceptionContext context)
