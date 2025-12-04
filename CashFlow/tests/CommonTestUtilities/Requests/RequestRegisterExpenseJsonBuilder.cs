9e6ae038df06b6bd7d554a4e4022d3a35f18d6a1
Gilson da Gama <gilsondagama@hotmail.com>
2025-12-04 12:19:10 -0300

    CashFlow: Criando uma requisição com dados falsos

---
+++ CashFlow/tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public RequestRegisterExpenseJson Build()
+    {
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(r => r.Title, f => f.Commerce.ProductName())
+            .RuleFor(r => r.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, f => f.Date.Past())
+            .RuleFor(r => r.Amount, f => f.Finance.Amount(min: 1, max: 1000))
+            .RuleFor(r => r.PaymentType, f => f.PickRandom<PaymentType>());
+    }
+}
