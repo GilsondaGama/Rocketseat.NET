ca3501cfb659dc1fa45794a2423a7b08c2fe4ec7
Gilson da Gama <gilsondagama@hotmail.com>
2025-12-16 10:31:42 -0300

    CashFlow: Concluindo os testes para o Validator

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -10,9 +10,7 @@ public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpense
     {
         RuleFor(x => x.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
         RuleFor(x => x.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
-        RuleFor(x => x.Date)
-            .LessThanOrEqualTo(DateTime.UtcNow)
-            .WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE);
+        RuleFor(x => x.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE);
         RuleFor(x => x.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
     }
 }
