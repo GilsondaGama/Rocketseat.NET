0fecefef056198e859d28847200189be01af113b
Gilson da Gama <gilsondagama@hotmail.com>
2025-12-29 11:11:14 -0300

    CashFlow: Configurando Injeção de dependências para o repositório

---
+++ CashFlow/src/CashFlow.Infrastructure/DependeceInjectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAcess.Expenses;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashFlow.Infrastructure;
+
+public static class DependeceInjectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IExpensesRepository, ExpensesRepository>();
+    }
+}
